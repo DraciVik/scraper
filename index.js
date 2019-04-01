2999342a2bef3cf1a6410cc6a3137b6877d2ab85
DraciVik <viktor.stojanov90@gmail.com>
2019-04-01 23:22:22 +0200

    Stuck on getting Instagram data. TODO: Get instagram data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { getHTML, getTwitterFollowers } from './lib/scraper';
+import { getHTML, getTwitterFollowers, getInstagramFollowers } from './lib/scraper';
 
 async function go() {
-        const html = await getHTML('https://twitter.com/DraciVik');
-        const twCount = await getTwitterFollowers(html);
-        console.log(`You have ${twCount} followers`);
+        // const html = await getHTML('https://twitter.com/DraciVik');
+        // const twCount = await getTwitterFollowers(html);
+        // console.log(`You have ${twCount} followers`);
+
+        const followers = await getInstagramFollowers('DraciVik');
 }
 
 go();
