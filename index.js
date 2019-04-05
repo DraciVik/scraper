e08dd931fcc050b3d7909ce05ae7d60834d33520
DraciVik <viktor.stojanov90@gmail.com>
2019-04-05 23:23:51 +0200

    added a cron job according to Wes Bos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import { getInstagramCount, getTwitterCount } from './lib/scraper';
 import db from './lib/db';
+import './lib/cron';
 
 const app = express();
 
@@ -10,13 +11,6 @@ console.log(db);
 app.get('/scrape', async (req, res, next) => {
         console.log('Scraping!!!');
         const [iCount, tCount] = await Promise.all([getInstagramCount(), getTwitterCount()]);
-        console.log(iCount, tCount);
-        db.get('twitter')
-                .push({ date: Date.now(), count: tCount })
-                .write();
-        db.get('instagram')
-                .push({ date: Date.now(), count: iCount })
-                .write();
         res.json({ iCount, tCount });
 });
 
