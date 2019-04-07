c256646e8076ecb1a8ce6eef3bbf7be5f9cc5bdc
DraciVik <viktor.stojanov90@gmail.com>
2019-04-08 00:27:51 +0200

    Finished day 3. I'm getting an error I need to fix

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ import { getInstagramCount, getTwitterCount } from './lib/scraper';
 import db from './lib/db';
 import './lib/cron';
 
-const app = express();
+const cors = require('cors');
 
-console.log(db);
+const app = express();
+app.use(cors());
 
 app.get('/scrape', async (req, res, next) => {
         console.log('Scraping!!!');
@@ -22,4 +23,6 @@ app.get('/data', async (req, res, next) => {
         // Respond with data
 });
 
-app.listen(2093, () => console.log(`Example App running on port http://localhost:2093`));
+app.listen(2093, () => {
+        console.log(`Example App running on port http://localhost:2093`);
+});
--- frontend/components/Data.js
+++ frontend/components/Data.js
@@ -1,12 +1,19 @@
 import { useContext } from 'react';
+import { distanceInWords } from 'date-fns';
 import { ScrapeContext } from './ScrapeContext';
 
 export default function Data() {
-        const scrapeData = useContext(ScrapeContext);
-        console.log(scrapeData);
+        const { scrapes } = useContext(ScrapeContext);
         return (
                 <div>
                         <h2>Your Data:</h2>
+                        <ul>
+                                {scrapes.twitter.map(scrape => (
+                                        <li key={scrape.date}>
+                                                {scrape.count}-{distanceInWords(new Date(scrape.date), new Date())}
+                                        </li>
+                                ))}
+                        </ul>
                 </div>
         );
 }
